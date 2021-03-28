47696293ec9cd79a3a5b8d3e64ec29768d5cfc80
Monica <monicawang.1997@gmail.com>
2021-03-27 22:08:22 -0700

    Create EmployeeSQL.sql

---
+++ EmployeeSQL.sql
@@ -0,0 +1,133 @@
+drop table Employees;
+drop table Departments;
+drop table Department_Employees;
+drop table Department_Manager;
+drop table Salaries;
+drop table Titles;
+
+-- DATA ENGINEERING
+
+CREATE TABLE Employees (
+    emp_no INT   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    gender VARCHAR   NOT NULL,
+    hire_date date   NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+SELECT * from Employees;
+
+CREATE TABLE Departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+select * from Departments;
+
+CREATE TABLE Department_Employees (
+    emp_no INT   NOT NULL,
+	dept_no VARCHAR   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
+select * from Department_Employees;
+
+CREATE TABLE Department_Manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no int   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
+SELECT * from Department_Manager;
+
+
+CREATE TABLE Salaries (
+    emp_no int   NOT NULL,
+	salary INTEGER   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,    
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+SELECT * from Salaries;
+
+
+CREATE TABLE Titles (
+	emp_no int   NOT NULL,
+    title VARCHAR   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
+SELECT * from Titles;
+
+
+-- 1
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.gender,
+Salaries.salary
+FROM Employees
+INNER JOIN Salaries ON
+Employees.emp_no = Salaries.emp_no;
+
+-- 2
+SELECT emp_no, first_name, last_name, hire_date from Employees
+WHERE hire_date >= '1985-12-31'
+AND hire_date < '1987-01-01';
+
+-- 3
+SELECT Department_Manager.dept_no, 
+	   Departments.dept_name,
+	   Department_Manager.emp_no,
+	   Employees.last_name,
+	   Employees.first_name,
+	   Department_Manager.from_date, 
+	   Department_Manager.to_date 
+FROM Department_Manager
+INNER JOIN Departments ON
+Department_Manager.dept_no = Departments.dept_no
+INNER JOIN Employees ON
+Department_Manager.emp_no = Employees.emp_no;
+
+-- 4
+SELECT Employees.emp_no, 
+	   Employees.last_name, 
+	   Employees.first_name,
+	   Departments.dept_name
+FROM Employees
+INNER JOIN Department_Manager ON
+Employees.emp_no = Department_Manager.emp_no
+INNER JOIN Departments ON
+Department_Manager.dept_no = Departments.dept_no;
+
+-- 5
+SELECT * FROM Employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- 6
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Employees AS e
+JOIN Department_Employees ON
+e.emp_no = Department_Employees.emp_no
+INNER JOIN Departments AS d ON
+Department_Employees.dept_no = d.dept_no
+WHERE dept_name = 'Sales';
+
+-- 7
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Employees AS e
+JOIN Department_Employees ON
+e.emp_no = Department_Employees.emp_no
+INNER JOIN Departments AS d ON
+Department_Employees.dept_no = d.dept_no
+WHERE dept_name = 'Sales' OR 
+	  dept_name = 'Development';
+
+-- 8
+SELECT last_name, COUNT(last_name) FROM Employees
+GROUP BY last_name
+ORDER BY count(last_name) desc;
